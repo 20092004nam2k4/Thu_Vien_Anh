58d8464ed6e61aa00b5b115f142047827d087c9d
NguyenQuanBao <114391082+NguyenQuanBao@users.noreply.github.com>
2024-03-15 15:35:31 +0700

    Add files via upload

---
+++ PictureLib/src/main/java/com/example/picturelib/PictureLibApplication.java
@@ -0,0 +1,13 @@
+package com.example.picturelib;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PictureLibApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PictureLibApplication.class, args);
+    }
+
+}
